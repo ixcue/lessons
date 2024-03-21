30d9c2566adcc629fe49bc5e8c246a98aabafcdf
ixcue <shershnyov.ra@gmail.com>
2024-03-21 18:27:59 +0200

    add new solution for home lesson 8

--- home_lesson_8.py
+++ home_lesson_8.py
@@ -1,12 +1,16 @@
-lst = [0, 1, 0, 12, 3]
-# [0]
+# lst = [0, 1, 0, 12, 3]
+lst = []
 # [1, 0, 13, 0, 0, 0, 5]
 # [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 
 a = 0
-for num in lst:
-    if num % 2 == 0:
-        a += num
+if len(lst) == 0:  # Перевірка порожнього масиву
+    lst = 0
+    print(lst)
 
-b = a*lst[-1]
-print(b)
+else:
+    for num in lst:
+        if num % 2 == 0:
+            a += num
+    b = a * lst[-1]
+    print(b)
